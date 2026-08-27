743379e6a5f4a50ec0ad0df8f58f2bdb858741d5
Yma Khaliya L. Basio <ymakhaliya05@gmail.com>
2026-08-27 15:06:38 +0000

    Done with atm_amount

---
+++ m1/practice8/basio_atm_account.py
@@ -0,0 +1,37 @@
+class Account:
+
+    def __init__(self, name, starting_balance):
+        self.account_name = name
+        self._balance = starting_balance
+
+    def check_balance(self):
+        print(f"Current Balance: ₱{self._balance:.2f}")
+
+    def deposit(self, amount):
+ 
+        if amount > 0:
+            self._balance = self._balance + amount
+            return True
+ 
+        else:
+            return False
+
+
+account = Account("Yma Basio", 10000.00)
+account.check_balance()
+account.deposit(1000)
+account.check_balance()
+
+""" 
+######### Learning Signature ######### 
+Programmed by: Yma Khaliya L. Basio
+Date Submitted: August 28, 2026
+ 
+Program Description: This program creates an ATM account using a class that will store the account name and balance and allows the user to check the balance and make a deposit.
+Reflection: I learned how to use a class, attributes and methods to organize related data and functions in one object.
+ 
+AI Usage
+[X] No AI Assistance – Completed independently without AI.
+[ ] AI as Support Tool – Used AI for explanations, syntax, or minor corrections.
+[ ] AI as Collaborative Partner – Used AI to design, structure, or co-create significant code.
+"""
